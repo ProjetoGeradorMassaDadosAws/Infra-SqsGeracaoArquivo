27b790887a329aa85ae86c4b2d7ed1e9ab7c6f46
Igor <igorcsaraiva@hotmail.com>
2022-03-29 22:30:55 -0300

    adição de variaveis

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue" "sqs_geracao_arquivo" {
-  name                      = "SqsGeracaoArquivo"
+  name                      = var.QueueName
   delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
